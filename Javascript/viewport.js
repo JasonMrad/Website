992131e018425d224cbc35e349ecd4115f4c277d
JasonMrad <163637645+JasonMrad@users.noreply.github.com>
2024-09-15 00:44:01 +0300

    Update viewport.js

--- Javascript/viewport.js
+++ Javascript/viewport.js
@@ -5,7 +5,7 @@ const mediaQuery = window.matchMedia("(max-width: 943px)");
 
 if (mediaQuery.matches) {
   
-  metaDescription.setAttribute("content", "width=device-width, initial-scale=0.35");
+  metaDescription.setAttribute("content", "width=device-width, initial-scale=0.35, user-scalable=yes"");
 } else {
  
   metaDescription.setAttribute("content", "width=device-width, initial-scale=1");
