d569aaf6801f7a6c4b49e3e3cfc29697441b920f
JasonMrad <163637645+JasonMrad@users.noreply.github.com>
2024-09-15 00:24:03 +0300

    Add files via upload

---
+++ Javascript/viewport.js
@@ -0,0 +1,20 @@
+
+function updateMetaTag() {
+const metaDescription = document.querySelector('meta[name="viewport"]');
+const mediaQuery = window.matchMedia("(max-width: 943px)");
+
+if (mediaQuery.matches) {
+  
+  metaDescription.setAttribute("content", "width=device-width, initial-scale=0.35");
+} else {
+ 
+  metaDescription.setAttribute("content", "width=device-width, initial-scale=1");
+}
+}
+
+// Run on page load
+window.addEventListener('load', updateMetaTag);
+
+// Run on window resize
+window.addEventListener('resize', updateMetaTag);
+
