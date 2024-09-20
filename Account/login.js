be5b056a8f3b3bdb800e8eaf40fd3f28244199b5
JasonMrad <163637645+JasonMrad@users.noreply.github.com>
2024-09-21 00:44:44 +0300

    Update login.js

--- Account/login.js
+++ Account/login.js
@@ -3,7 +3,9 @@ const register=document.getElementById("register-form");
 const container=document.getElementsByClassName("container");
 const loginTab=document.getElementById("login-tab");
 const createAccountTab=document.getElementById("createAccount-tab");
-
+if(window.innerWidth<950){
+window.onload=document.getElementsByClassName('main-section')[0].style.height="450px"; // Selects the first matching element
+}
 function showLoginForm(){
     register.style.display="none";
     login.style.display="block";
@@ -12,11 +14,13 @@ function showLoginForm(){
     loginTab.style.borderBottom = "2px solid black"; // Add border to Login tab
 loginTab.style.fontWeight="bold";
 createAccountTab.style.fontWeight="normal";
+if(window.innerWidth<950){document.getElementsByClassName('main-section')[0].style.height="450px";}
 }
 function showRegisterForm(){
     login.style.display="none";
    register.style.display="block";
-    container[0].setAttribute('style', 'overflow-y: auto; overflow-x: hidden;max-height:383px;');
+    container[0].setAttribute('style', 'overflow-y: auto; overflow-x: hidden;');
+    if(window.innerWidth<950) {document.getElementsByClassName('main-section')[0].style.height="860px";}
     createAccountTab.style.borderBottom = "2px solid black"; // Remove border from Create Account tab
     loginTab.style.borderBottom = "none"; // Add border to Login tab
     loginTab.style.fontWeight="normal";
