b0ea3b45ab6b85e7b394715bb04064e01412d4f0
JasonMrad <163637645+JasonMrad@users.noreply.github.com>
2024-10-03 14:10:10 +0300

    Update product-redirection.js

--- Products/product-redirection.js
+++ Products/product-redirection.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.product-card').forEach(card => {
     card.addEventListener('click', function() {
         // Redirect to special.html when a product card is clicked
-        window.location.href = 'test.html';
+        window.location.href = 'product-redirection.html';
     });
 });
